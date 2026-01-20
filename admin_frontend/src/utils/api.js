f92142bc14ccc5009dee155d250297ec54ce26e9
meet <sitesapp091@gmail.com>
2026-01-20 10:11:03 +0530

    API COMPLETED FINALLY

--- admin_frontend/src/utils/api.js
+++ admin_frontend/src/utils/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000',
+  // baseURL: 'http://localhost:3000',
+  baseURL: 'https://mern-watch-store.onrender.com',
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json'
